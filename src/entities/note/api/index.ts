e81c7c54d4b4920f9e478b79ea256b5311bc0dc1
nicknameisavailable <iamgenii@yandex.ru>
2023-11-15 15:20:44 +0300

    note/api

---
+++ src/entities/note/api/index.ts
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Notes } from '../model/index'
+
+const getAccessToken = () => {
+	if (typeof window === 'undefined') {
+		return null
+	}
+	return localStorage.getItem('access')
+}
+export const NoteApi = createApi({
+	reducerPath: 'NoteApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://127.0.0.1:8000/api/notes',
+		headers: {
+			Authorization: `Bearer ${getAccessToken()}`,
+		},
+	}),
+	endpoints: builder => ({
+		getByVault: builder.query<Notes, string>({
+			query: (id: string | undefined) => ({
+				url: `/byvault?id=${id}`,
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer ${getAccessToken()}`,
+				},
+			}),
+		}),
+	}),
+})
+
+export const { useGetByVaultQuery } = NoteApi
